f72012f9d75774061d74b89e9493e2b87070645d
GuyTito <profkofi@gmail.com>
2021-04-07 15:49:28 +0000

    renamed 'home' to 'admin'
    - renamed 'home' to 'admin'
    - created homepage to view the league table
    - created folders for match, player, team and season

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Admin Page') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <div class="mb-2">
+                        <a href="{{ route('admin') }}">
+                            Save match
+                        </a>
+                    </div>
+                    <div class="mb-2">
+                        <a href="{{ route('admin') }}">
+                            Add football player
+                        </a>
+                    </div>
+                    <div class="mb-2">
+                        <a href="{{ route('admin') }}">
+                            Add football team
+                        </a>
+                    </div>
+                    <div>
+                        <a class="" href="{{ route('admin') }}">
+                            Create football season
+                        </a>
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
