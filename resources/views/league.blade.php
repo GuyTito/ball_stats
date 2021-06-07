3b63a719c81292ccc60e0cb2a7d3b59a074c6798
GuyTito <profkofi@gmail.com>
2021-06-07 20:50:57 +0000

    toggle tabs for league page

--- resources/views/league.blade.php
+++ resources/views/league.blade.php
@@ -17,7 +17,7 @@
                 </div>
 
                 <div class="card-body">
-                    <div class="dropdown">Season: 
+                    <div class="dropdown mb-3">Season: 
                         <a class="text-secondary" href="{{ route('season.show', $current_season) }}">
                             {{ Carbon\Carbon::parse($current_season->start_date)->toFormattedDateString() }} -
                             {{ Carbon\Carbon::parse($current_season->end_date)->toFormattedDateString() }}
@@ -36,120 +36,141 @@
                         @endforeach
                     </div>
 
-                
-                    <div class="mt-3">
-                        <h3>standings</h3>
-                        <div class="table-responsive-sm">
-                            <table class="table table-hover table-sm">
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Club</th>
-                                        <th>MP</th>
-                                        <th>W</th>
-                                        <th>D</th>
-                                        <th>L</th>
-                                        <th>Pts</th>
-                                        <th>GS</th>
-                                        <th>GC</th>
-                                        <th>GD</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @forelse ($standings as $team)
-                                        <tr>
-                                            <td>{{$loop->iteration}}</td>
-                                            <td><a href="{{ route('team.show', $team['team']) }}">{{$team['team']->name}}</a></td> 
-                                            <td>{{$team['mp']}}</td> 
-                                            <td>{{$team['w']}}</td> 
-                                            <td>{{$team['d']}}</td> 
-                                            <td>{{$team['l']}}</td> 
-                                            <td><strong>{{$team['pts']}}</strong></td>
-                                            <td>{{$team['gs']}}</td> 
-                                            <td>{{$team['gc']}}</td> 
-                                            <td>{{$team['gc'] - $team['gc']}}</td> 
-                                        </tr>
-                                    @empty
-                                        <p>No team created.</p> 
-                                    @endforelse
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-
-                    <div class="mt-3">
-                        <h3>matches</h3>
-                        @forelse  ($matches as $match)
-                            <div class="mb-2">
-                                <a href="{{ route('match.show', $match) }}">
-                                    <div>{{ Carbon\Carbon::parse($match->date_played)->toFormattedDateString() }}</div>
-                                    {{$match->home_team->name}} {{$match->home_team_score}} - 
-                                    {{$match->away_team_score}} {{$match->away_team->name}}
-                                </a>
+                    <div class="bs-example">
+                        <ul class="nav nav-tabs nav-fill" id="stats">
+                            <li class="nav-item">
+                                <a href="#standings" class="nav-link" data-toggle="tab">Standings</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#goals" class="nav-link" data-toggle="tab">Goals</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#assists" class="nav-link" data-toggle="tab">Assists</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#matches" class="nav-link" data-toggle="tab">Matches</a>
+                            </li>
+                        </ul>
+                        <div class="tab-content">
+                            <div class="tab-pane fade" id="standings">
+                                <h4 class="mt-2">Standings</h4>
+                                <div class="table-responsive-sm">
+                                    <table class="table table-hover table-sm">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Club</th>
+                                                <th>MP</th>
+                                                <th>W</th>
+                                                <th>D</th>
+                                                <th>L</th>
+                                                <th>Pts</th>
+                                                <th>GS</th>
+                                                <th>GC</th>
+                                                <th>GD</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @forelse ($standings as $team)
+                                                <tr>
+                                                    <td>{{$loop->iteration}}</td>
+                                                    <td><a href="{{ route('team.show', $team['team']) }}">{{$team['team']->name}}</a></td> 
+                                                    <td>{{$team['mp']}}</td> 
+                                                    <td>{{$team['w']}}</td> 
+                                                    <td>{{$team['d']}}</td> 
+                                                    <td>{{$team['l']}}</td> 
+                                                    <td><strong>{{$team['pts']}}</strong></td>
+                                                    <td>{{$team['gs']}}</td> 
+                                                    <td>{{$team['gc']}}</td> 
+                                                    <td>{{$team['gc'] - $team['gc']}}</td> 
+                                                </tr>
+                                            @empty
+                                                <p>No team created.</p> 
+                                            @endforelse
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
-                        @empty
-                           <p>No match event recorded.</p> 
-                        @endforelse
-                    </div>
-
-                    <div class="mt-3">
-                        <h3>goals</h3>
-                        <div class="table-responsive-sm">
-                            <table class="table table-hover table-sm">
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Player</th>
-                                        <th>Club</th>
-                                        <th>Goals</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @forelse ($goals as $player)
-                                        <tr>
-                                            <td>{{$loop->iteration}}</td>
-                                            <td><a href="{{ route('player.show', $player['player']) }}">{{$player['player']->name}}</a></td> 
-                                            <td>{{$player['team']}}</td>
-                                            <td>{{$player['goals']}}</td>
-                                        </tr>
+                            <div class="tab-pane fade" id="goals">
+                                <h4 class="mt-2">Goals</h4>
+                                <div class="table-responsive-sm">
+                                    <table class="table table-hover table-sm">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Player</th>
+                                                <th>Club</th>
+                                                <th>Goals</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @forelse ($goals as $player)
+                                                <tr>
+                                                    <td>{{$loop->iteration}}</td>
+                                                    <td><a href="{{ route('player.show', $player['player']) }}">{{$player['player']->name}}</a></td> 
+                                                    <td>{{$player['team']}}</td>
+                                                    <td>{{$player['goals']}}</td>
+                                                </tr>
+                                            @empty
+                                                <p>No goals recorded.</p>
+                                            @endforelse
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="assists">
+                                <h4 class="mt-2">Assists</h4>
+                                <div class="table-responsive-sm">
+                                    <table class="table table-hover table-sm">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Player</th>
+                                                <th>Club</th>
+                                                <th>Assists</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @forelse ($assists as $player)
+                                                <tr>
+                                                    <td>{{$loop->iteration}}</td>
+                                                    <td><a href="{{ route('player.show', $player['player']) }}">{{$player['player']->name}}</a></td> 
+                                                    <td>{{$player['team']}}</td>
+                                                    <td>{{$player['assists']}}</td>
+                                                </tr>
+                                            @empty
+                                                <p>No assists recorded.</p>
+                                            @endforelse
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="matches">
+                                <h4 class="mt-2">Matches</h4>
+                                <div>
+                                    @forelse  ($matches as $match)
+                                        <div class="mb-2">
+                                            <a href="{{ route('match.show', $match) }}">
+                                                <div>{{ Carbon\Carbon::parse($match->date_played)->toFormattedDateString() }}</div>
+                                                {{$match->home_team->name}} {{$match->home_team_score}} - 
+                                                {{$match->away_team_score}} {{$match->away_team->name}}
+                                            </a>
+                                        </div>
                                     @empty
-                                        <p>No goals recorded.</p>
+                                        <p>No match event recorded.</p> 
                                     @endforelse
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-
-                    <div class="mt-3">
-                        <h3>assists</h3>
-                        <div class="table-responsive-sm">
-                            <table class="table table-hover table-sm">
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Player</th>
-                                        <th>Club</th>
-                                        <th>Assists</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @forelse ($assists as $player)
-                                        <tr>
-                                            <td>{{$loop->iteration}}</td>
-                                            <td><a href="{{ route('player.show', $player['player']) }}">{{$player['player']->name}}</a></td> 
-                                            <td>{{$player['team']}}</td>
-                                            <td>{{$player['assists']}}</td>
-                                        </tr>
-                                    @empty
-                                        <p>No assists recorded.</p>
-                                    @endforelse
-                                </tbody>
-                            </table>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <script>
+        $(document).ready(function(){ 
+            $("#stats li:eq() a").tab('show'); // show 1st tab on page load
+        });
+    </script>
 </div>
 @endsection
