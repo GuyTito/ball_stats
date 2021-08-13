8f3265489debf42363b8378324c2fda57a11e1b0
GuyTito <profkofi@gmail.com>
2021-08-13 19:07:57 +0000

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,79 +28,93 @@
   <link rel="manifest" href="/site.webmanifest"> --}}
 </head>
 
-<body>
-  <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-      <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-          {{ config('app.name', 'Laravel') }}
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+<body class="d-flex flex-column min-vh-100">
+  <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+      <a class="navbar-brand" href="{{ url('/') }}">
+        {{ config('app.name', 'Laravel') }}
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <!-- Left Side Of Navbar -->
-          <ul class="navbar-nav mr-auto">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Left Side Of Navbar -->
+        <ul class="navbar-nav mr-auto">
 
-          </ul>
+        </ul>
 
-          <!-- Right Side Of Navbar -->
-          <ul class="navbar-nav ml-auto">
-            <!-- Dark Mode Switch -->
-            <li class="nav-link">
-              <div class="nav-item custom-control custom-switch mr-2">
-                <input type="checkbox" class="custom-control-input" id="darkSwitch" />
-                <label class="custom-control-label p-0" for="darkSwitch">Theme</label>
-              </div>
-            </li>
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
+          <!-- Dark Mode Switch -->
+          <li class="nav-link">
+            <div class="nav-item custom-control custom-switch mr-2">
+              <input type="checkbox" class="custom-control-input" id="darkSwitch" />
+              <label class="custom-control-label p-0" for="darkSwitch">Theme</label>
+            </div>
+          </li>
 
-            <!-- Authentication Links -->
-            @guest
-              @if (Route::has('login'))
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                </li>
-              @endif
+          <!-- Authentication Links -->
+          @guest
+            @if (Route::has('login'))
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+              </li>
+            @endif
 
-              @if (Route::has('register'))
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                </li>
-              @endif
-            @else
-              <li class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
-                  aria-haspopup="true" aria-expanded="false" v-pre>
-                  {{ Auth::user()->name }}
+            @if (Route::has('register'))
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+              </li>
+            @endif
+          @else
+            <li class="nav-item dropdown">
+              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                {{ Auth::user()->name }}
+              </a>
+
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('admin') }}">
+                  {{ __('Admin') }}
                 </a>
 
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="{{ route('admin') }}">
-                    {{ __('Admin') }}
-                  </a>
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                  onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                  {{ __('Logout') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                  @csrf
+                </form>
 
-                  <a class="dropdown-item" href="{{ route('logout') }}"
-                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                  </a>
-                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                  </form>
-
-                </div>
-              </li>
-            @endguest
-          </ul>
-        </div>
+              </div>
+            </li>
+          @endguest
+        </ul>
       </div>
-    </nav>
+    </div>
+  </nav>
 
-    <main class="py-4">
-      @yield('content')
-    </main>
-  </div>
+  <main class="py-4 wrapper flex-grow-1">
+    @yield('content')
+  </main>
+
+  <footer class="text-center pb-3">
+    <a
+      class="text-decoration-none text-muted mx-3"
+      href="https://twitter.com/QuistKofi"
+      target="blank"
+      >My Twitter</a
+    >
+    <a
+      class="text-decoration-none text-muted mx-4"
+      href="https://github.com/GuyTito/ball_stats"
+      target="blank"
+      >Source</a
+    >
+  </footer>
+
   <script src="/js/dark-mode-switch.min.js"></script>
 </body>
 
